27882de6f8e5b90ff5b4ca77136421279d0011b0
kritikaparmar-programmer <kprogrammer07@gmail.com>
2020-12-11 23:21:46 +0530

    Data Analysis

---
+++ datascientist/eda/eda.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+def eda(df):
+    print("Exploratory Data Analysis for Data:\n")
+    print("Head of Data: ", df.head())
+    print("===========================================================================")
+    print("Types present in data frame: ", df.dtypes)
+    print("===========================================================================")
+    print("Correlation between different columns of Dataframe: ", df.corr())
+    print("===========================================================================")
+    print("Null values: ", df.isnull().sum())
+    print("===========================================================================")
+    sns.heatmap(df.isnull(), yticklabels=False, cbar=False, cmap='viridis')
+    sns.pairplot(df)
